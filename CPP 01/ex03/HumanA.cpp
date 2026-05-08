c2322371c51e6cd623c1d7214ed4020648faa3ee
Angeliere <81757446+Angeliere@users.noreply.github.com>
2026-05-08 14:42:31 +0200

    Delete CPP 01/ex03 directory

--- CPP 01/ex03/HumanA.cpp
+++
@@ -1,9 +0,0 @@
-#include <HumanA.hpp>
-void HumanA::attack()
-{
-	std::cout << _name << " attacks with their " << _weapon << std::endl;
-}
-
-HumanA::HumanA(std::string const name , Weapon &weapon) : _name(name), _weapon(weapon) {};
-
-
